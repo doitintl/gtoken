6bb6696367383dc6df0c65688d7e964cbf94886c
Alexei Ledenev <alexei.led@gmail.com>
2019-12-04 13:59:53 +0200

    refresh token and write to file

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -16,34 +19,45 @@ import (
 	iamcredentials "google.golang.org/api/iamcredentials/v1"
 )
 
-func generateIDToken(c *cli.Context) error {
-	log.Println("getting token ...")
-	ctx := context.Background()
+var (
+	mainCtx context.Context
+)
 
-	creds, err := google.FindDefaultCredentials(ctx)
+func getServiceAccountID(ctx context.Context) (string, error) {
+	// handle the 'refresh token' command
+	cx, cancel := context.WithCancel(ctx)
+	// cancel current context on exit
+	defer cancel()
+	creds, err := google.FindDefaultCredentials(cx)
 	if err != nil {
-		return fmt.Errorf("failed to find default credentials: %s", err)
+		return "", fmt.Errorf("failed to find default credentials: %s", err)
 	}
-
 	credsMap := make(map[string]interface{})
 	err = json.Unmarshal(creds.JSON, &credsMap)
 	if err != nil {
-		return fmt.Errorf("failed to parse credentials JSON: %s", err)
+		return "", fmt.Errorf("failed to parse credentials JSON: %s", err)
 	}
-
-	var serviceAccountID string
-	var ok bool
-	if serviceAccountID, ok = credsMap["client_id"].(string); !ok {
-		return errors.New("failed to find service account ID")
+	if id, ok := credsMap["client_id"].(string); ok {
+		return id, nil
 	}
+	return "", errors.New("failed to find service account ID")
+}
 
-	client, err := google.DefaultClient(ctx, iamcredentials.CloudPlatformScope)
+func generateIDToken(ctx context.Context, serviceAccountID string) (string, error) {
+	log.Println("generating a new ID token")
+
+	// handle the 'refresh token' command
+	cx, cancel := context.WithCancel(ctx)
+	// cancel current context on exit
+	defer cancel()
+
+	client, err := google.DefaultClient(cx, iamcredentials.CloudPlatformScope)
 	if err != nil {
-		return err
+		return "", err
 	}
 	iamCredentialsClient, err := iamcredentials.New(client)
 	if err != nil {
-		return fmt.Errorf("failed to get iam credentials client: %s", err.Error())
+		return "", fmt.Errorf("failed to get iam credentials client: %s", err.Error())
 	}
 	generateIDTokenResponse, err := iamCredentialsClient.Projects.ServiceAccounts.GenerateIdToken(
 		fmt.Sprintf("projects/-/serviceAccounts/%s", serviceAccountID),
@@ -51,33 +65,141 @@ func generateIDToken(c *cli.Context) error {
 			Audience: "32555940559.apps.googleusercontent.com",
 		},
 	).Do()
-
 	if err != nil {
-		return fmt.Errorf("failed to generate ID token: %s", err.Error())
+		return "", fmt.Errorf("failed to generate ID token: %s", err.Error())
 	}
+	log.Println("successfully generated ID token")
+	return generateIDTokenResponse.Token, nil
+}
 
+func getTokenDuration(jwtToken string) (time.Duration, error) {
 	// parse JWT token
 	parser := jwt.Parser{UseJSONNumber: true, SkipClaimsValidation: true}
-	token, err := parser.Parse(generateIDTokenResponse.Token, nil)
+	token, err := parser.Parse(jwtToken, nil)
 	//jwt.Parse(generateIDTokenResponse.Token, nil)
 	if claims, ok := token.Claims.(jwt.MapClaims); ok {
 		unixTime, err := claims["exp"].(json.Number).Int64()
 		if err != nil {
-			return fmt.Errorf("failed to convert expire date: %s", err.Error())
+			return 0, fmt.Errorf("failed to convert expire date: %s", err.Error())
 		}
-		log.Printf("token will expire on: %s", time.Unix(unixTime, 0))
+		return time.Unix(unixTime, 0).Sub(time.Now()), nil
 	} else {
-		return fmt.Errorf("failed to get claims from ID token: %s", err.Error())
+		return 0, fmt.Errorf("failed to get claims from ID token: %s", err.Error())
+	}
+}
+
+func writeToken(token string, fileName string) error {
+	// this is a slice of io.Writers we will write the file to
+	var writers []io.Writer
+
+	// if no file provided
+	if len(fileName) == 0 {
+		writers = append(writers, os.Stdout)
 	}
 
+	// if DestFile was provided, lets try to create it and add to the writers
+	if len(fileName) > 0 {
+		file, err := os.Create(fileName)
+		if err != nil {
+			return fmt.Errorf("failed to create token file: %s; error: %s", fileName, err.Error())
+		}
+		writers = append(writers, file)
+		defer file.Close()
+	}
+	// MultiWriter(io.Writer...) returns a single writer which multiplexes its
+	// writes across all of the writers we pass in.
+	dest := io.MultiWriter(writers...)
+	// write to dest the same way as before, copying from the Body
+	if _, err := io.WriteString(dest, token); err != nil {
+		return fmt.Errorf("failed to write token: %s", err.Error())
+	}
 	return nil
+}
+
+func generateIDTokenCmd(c *cli.Context) error {
+	// find out active Service Account ID
+	serviceAccountID, err := getServiceAccountID(mainCtx)
+	if err != nil {
+		return err
+	}
+	// initial duration to 1ms
+	duration := time.Millisecond
+	timer := time.NewTimer(duration).C
+	for {
+		// wait for next timer tick or cancel
+		select {
+		case <-mainCtx.Done():
+			return nil // avoid goroutine leak
+		case <-timer:
+			// generate ID token
+			token, err := generateIDToken(mainCtx, serviceAccountID)
+			if err != nil {
+				return err
+			}
+			// write generated token to file or stdout
+			err = writeToken(token, c.String("file"))
+			if err != nil {
+				return err
+			}
+			// auto-refresh enabled
+			if c.Bool("refresh") {
+				// get token duration
+				duration, err = getTokenDuration(token)
+				// debug reduce duration
+				if err != nil {
+					return err
+				}
+				// reduce duration by 30s
+				duration = duration - 30*time.Second
+				log.Printf("refreshing token in %s", duration)
+				// reset timer
+				timer = time.NewTimer(duration).C
+			} else {
+				return nil // avoid goroutine leak
+			}
+		}
+	}
+}
+
+func init() {
+	// handle termination signal
+	mainCtx = handleSignals()
+}
+
+func handleSignals() context.Context {
+	// Graceful shut-down on SIGINT/SIGTERM
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	// create cancelable context
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		defer cancel()
+		sid := <-sig
+		log.Printf("received signal: %d\n", sid)
+		log.Println("canceling token refresh ...")
+	}()
+
+	return ctx
 }
 
 func main() {
 	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:  "refresh",
+				Value: true,
+				Usage: "auto refresh ID token before it expires",
+			},
+			&cli.StringFlag{
+				Name:  "file",
+				Usage: "write ID token into file",
+			},
+		},
 		Name:   "gtoken",
 		Usage:  "generate GCP ID token with current service account",
-		Action: generateIDToken,
+		Action: generateIDTokenCmd,
 	}
 
 	err := app.Run(os.Args)
